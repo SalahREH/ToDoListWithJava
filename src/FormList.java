73c7f73c03f516eb2532061398bf4f3500bdb4c1
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-03-01 14:54:01 +0100

    titles now are printed in the form

--- src/FormList.java
+++ src/FormList.java
@@ -3,6 +3,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FormList extends JFrame {
     private JButton AddButton;
@@ -11,20 +13,23 @@ public class FormList extends JFrame {
     private JTextArea tasksList;
 
 
-    public FormList(String title, Connection connection) {
-        super(title);
+    public FormList(String formTitle, Connection connection) {
+        super(formTitle);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
+        ArrayList<String> titles = new ArrayList<String>();
         try {
             Statement stmt  = connection.createStatement();
             String sql = "SELECT * " + "FROM lists";
-
             ResultSet rs    = stmt.executeQuery(sql);
+
             while (rs.next()) {
-                String titles = rs.getString("title");
-                System.out.println(titles);
+                String title = rs.getString("title");
+                System.out.println(title);
+                titles.add(title);
             }
+            tasksList.setText(String.valueOf(titles));
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
